cd1c81ffbf215561e92565112c21b1c0e741097b
David Njoroge <muriithid05@gmail.com>
2023-09-07 14:17:09 -0700

    On branch master 	modified:   101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - the main function
- *
- * Return: returns 1 after program executes
- */
-
+ *printing to the standard error
+ * Return: always 0(success)
+*/
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-    return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (0);
 }
