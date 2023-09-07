974f6ccd9bf193aa5359c48398a787743b0d83e5
David Njoroge <muriithid05@gmail.com>
2023-09-07 12:04:40 -0700

    On branch master 	modified:   5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+ * main - Entry 
+ *
+ * Description: Prints a string with proper grammar, but the outcome is a piece of art
+ *
+ * Return: 0 if successful
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
