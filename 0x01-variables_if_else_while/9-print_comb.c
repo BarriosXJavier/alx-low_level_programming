3bad31332946efe271e0b4398ff05a347b4aae71
David Njoroge <muriithid05@gmail.com>
2023-09-09 14:06:37 -0700

    On branch master 	modified:   9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-
 int main(void)
 {
-int i, j;
-
-    /* loop over the first digit from 0 to 9 */
+int i;
+    /* loop over the digits from 0 to 9 */
 for (i = 0; i <= 9; i++)
 {
-        /* loop over the second digit from i + 1 to 9 */
-for (j = i + 1; j <= 9; j++)
-{
-            /* print the pair of digits using putchar and adding '0' to convert to ASCII */
-            /* use a ternary operator to print a comma and a space or a newline depending on the pair */
-putchar(i + '0'), putchar(j + '0'), putchar((i == 8 && j == 9) ? '\n' : ',');
-            /* print a space using putchar if the pair is not the last one (8, 9) */
-if (i != 8 || j != 9)
+        /* print the digit using putchar and adding '0' to convert to ASCII */
+putchar(i + '0');
+        /* print a comma and a space using putchar if the digit is not the last one (9) */
+if (i != 9)
+putchar(',');
 putchar(' ');
 }
-}
+    /* print a newline character using putchar */
+putchar('\n');
     /* return 0 to indicate successful execution */
 return (0);
 }
