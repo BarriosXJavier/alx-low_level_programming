bede0254aee911ba5296129f7eb67aa957399f59
David Njoroge <muriithid05@gmail.com>
2023-09-09 13:33:30 -0700

    On branch master 	new file:   8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+8#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: 0 if successful
+ */
+int main(void)
+{
+int i;
+char c;
+i = 0;
+while (i < 10) /* loop for digits */
+{
+putchar(i + '0');
+i++;
+}
+c = 'a';
+while (c <= 'f') /* loop for letters */
+{
+putchar(c);
+c++;
