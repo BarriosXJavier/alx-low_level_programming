9ab83d37aa87316b48c1ad71a2741dab1b988c09
David Njoroge <muriithid05@gmail.com>
2023-09-09 12:38:14 -0700

    On branch master 	new file:   5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+/* Loop from 0 to 9 */
+for (i = 0; i < 10; i++)
+{
+      /** Print the number followed by a new line */
+printf("%d\n", i);
+}
+return (0);
+}
+
