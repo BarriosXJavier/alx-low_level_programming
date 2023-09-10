28b069b6c1950924dda00b867521d8a5e81bf792
David Njoroge <muriithid05@gmail.com>
2023-09-10 09:33:02 -0700

    On branch master 	modified:   100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - possible different combinations of two digits
+ * main - Entry point, prints all possible two-dig
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i, j;
 for (i = 0; i < 10; i++)
-    	{
+	{
 		for (j = i + 1; j < 10; j++)
-        {
-		putchar(i + '0');
-		putchar(j + '0');
-		if (i != 8 || j != 9)
-            	{
-                	putchar(',');
-                	putchar(' ');
-            	}
-        }
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
-putchar('\n');
-return (0);
+
+	putchar('\n');
+
+	return (0);
 }
 
