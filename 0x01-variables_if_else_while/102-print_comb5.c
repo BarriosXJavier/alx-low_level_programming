023108cb8523572250867babb0022706fe8c3f96
David Njoroge <muriithid05@gmail.com>
2023-09-11 02:49:50 -0700

    On branch master 	modified:   102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,33 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * print_two_digit - Print a two-digit number
- * @num: The number to be printed
- */
-void print_two_digit(int num) {
-    putchar(num / 10 + '0');
-    putchar(num % 10 + '0');
-}
-
-/**
- * main - Entry point of the program
- *
+ * main - combinations of two two-digit numbers
  * Return: Always 0 (Success)
  */
-int main() {
-    for (int i = 0; i < 100; i++) {
-        for (int j = i; j < 100; j++) {
-            print_two_digit(i);
-            putchar(' ');
-            print_two_digit(j);
+int main(void)
+{
+    int i, j;
 
-            if (i != 99 || j != 99) {
+    for (i = 0; i < 100; i++)
+    {
+        for (j = i + 1; j < 100; j++)
+        {
+            putchar((i / 10) + '0');
+            putchar((i % 10) + '0');
+            putchar(' ');
+            putchar((j / 10) + '0');
+            putchar((j % 10) + '0');
+
+            if (i < 98 || j < 99)
+            {
                 putchar(',');
                 putchar(' ');
             }
         }
     }
 
-    return 0;
+    putchar('\n');
+
+    return (0);
 }
 
