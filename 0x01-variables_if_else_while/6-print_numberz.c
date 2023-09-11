3072449310bf471e35ac40db20b1004164759f12
David Njoroge <muriithid05@gmail.com>
2023-09-11 16:39:51 -0700

    On branch master 	new file:   6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description: This program prints all single-digit numbers in base 10,
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit = 0; // Initialize a variable to store the current digit (0-9)
+while (digit <= 9) // Loop until digit reaches 9
+	{
+		putchar(digit + '0'); // Convert the digit to its ASCII character and print it
+		digit++; // Increment the digit for the next iteration
+	}
+
+putchar('\n'); // Print a newline character
+return (0); // Exit the program
+}
