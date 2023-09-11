fcaf0277a04024801c9dd07428fc73656370c449
David Njoroge <muriithid05@gmail.com>
2023-09-11 14:10:08 -0700

    On branch master 	modified:   102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,11 +7,12 @@ i *
  */
 int main(void)
 {
-int i, j;
-for (i = 0; i < 100; i++)
+    int i, j;
+
+    for (i = 0; i < 100; i++)
 	{
         for (j = i + 1; j < 100; j++)
-        {
+	{
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
 		putchar(' ');
@@ -22,10 +23,9 @@ for (i = 0; i < 100; i++)
 		putchar(',');
 		putchar(' ');
 	}
-        }
+	}
 	}
 
-	putchar('\n');
-
-	return (0);
+putchar('\n');
+return (0);
 }	
