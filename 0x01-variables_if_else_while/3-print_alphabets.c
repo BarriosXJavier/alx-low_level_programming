21573b9c41a67575aca45db19cabb522ea4f1319
David Njoroge <muriithid05@gmail.com>
2023-09-11 14:57:12 -0700

    On branch master 	modified:   3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,20 +6,18 @@
  */
 int main(void)
 {
-    int ch = 'a'; /* first lowercase letter */
-
-    while (ch <= 'Z') /* loop until last uppercase letter */
-    {
+int ch = 'a'; /* first lowercase letter */
+while (ch <= 'Z') /* loop until last uppercase letter */
+	{
         if (ch > 'z' && ch < 'A') /* print new line and reset ch */
-        {
-            putchar('\n');
-            ch = 'A';
-        }
-        putchar(ch); /* print current letter */
-        ch++; /* next letter */
-    }
-    putchar('\n'); /* final new line */
-
-    return (0);
+	{
+	putchar('\n');
+	ch = 'A';
+	}
+	putchar(ch); /* print current letter */
+	ch++; /* next letter */
+	}
+putchar('\n'); /* final new line */
+return (0);
 }
 
