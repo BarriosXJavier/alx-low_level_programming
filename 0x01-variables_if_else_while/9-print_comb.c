c62d04ccb45ec55bb0ee615b3117771fa3cffc4d
David Njoroge <muriithid05@gmail.com>
2023-09-11 17:11:06 -0700

    On branch master 	modified:   9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
+    int i, j;
     /* Iterate through the tens and ones digits */
-    for (int i = 0; i <= 9; i++) {
-        for (int j = 0; j <= 9; j++) {
+    for (i = 0; i <= 9; i++) {
+        for (j = 0; j <= 9; j++) {
             /* Print the tens digit (i) if not zero */
             if (i != 0 || j != 0) {
                 putchar(i + '0');    /* Convert to character and print */
@@ -16,4 +17,3 @@ int main() {
     }
     return 0;
 }
-
