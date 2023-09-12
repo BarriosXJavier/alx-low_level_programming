0c92484f99f725ba009e607e9e235cdd15f9fa25
David Njoroge <muriithid05@gmail.com>
2023-09-11 17:06:08 -0700

    On branch master 	modified:   9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
-/**
- * main - all possible combos of single-digit numbers
- */
-int main(void)
-{
-int i, j;
-for (i = 0; i < 10; i++)
-{
-for (j = i + 1; j < 10; j++)
-{
-putchar(i + '0');
-putchar(j + '0');
-if (i != 8 || j != 9)
-{
-putchar(',');
-}           
-}
-}
-return (0);
+int main() {
+    /* Iterate through the tens and ones digits */
+    for (int i = 0; i <= 9; i++) {
+        for (int j = 0; j <= 9; j++) {
+            /* Print the tens digit (i) if not zero */
+            if (i != 0 || j != 0) {
+                putchar(i + '0');    /* Convert to character and print */
+                putchar(',');         /* Print comma */
+                putchar(' ');         /* Print space */
+            }
+            putchar(j + '0');        /* Convert to character and print the ones digit */
+            putchar('\n');           /* Print newline to separate combinations */
+        }
+    }
+    return 0;
 }
 
