ce9053dc1eee89752b9596206c6b34cb42ed2cd5
David Njoroge <muriithid05@gmail.com>
2023-09-11 17:16:18 -0700

    On branch master 	modified:   9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,15 +5,20 @@ int main() {
     /* Iterate through the tens and ones digits */
     for (i = 0; i <= 9; i++) {
         for (j = 0; j <= 9; j++) {
-            /* Print the tens digit (i) if not zero */
+            /* Check if it's not the first combination */
             if (i != 0 || j != 0) {
-                putchar(i + '0');    /* Convert to character and print */
-                putchar(',');         /* Print comma */
-                putchar(' ');         /* Print space */
+                /* Print the two-digit number */
+                putchar(i * 10 + j);
+
+                /* Check if it's not the last combination */
+                if (i != 9 || j != 9) {
+                    putchar(',');    /* Print comma */
+                    putchar(' ');    /* Print space */
+                }
             }
-            putchar(j + '0');        /* Convert to character and print the ones digit */
-            putchar('\n');           /* Print newline to separate combinations */
         }
     }
+    putchar('\n'); /* Print newline after all combinations */
     return 0;
 }
+
