cd65537b18d5af7e05638baa8a760c88b52ed32d
David Njoroge <muriithid05@gmail.com>
2023-09-13 06:40:47 -0700

    On branch master 	modified:   2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,20 +4,16 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
+	char c;
+	int i = 0;
 
-	i = 0;
-	
-	while (i < 10)
+	while (i <= 0)
 	{
-		ch = '';
-		while (ch <= '2')
-		{
-			_putchar(ch);
-			ch++;
-		}
-		_putchar('\n');
-		i++;
+	for (c = 'a'; c <= '2'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
 	}
 }
