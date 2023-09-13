958e5faa89143651293445ea98448630842066c4
David Njoroge <muriithid05@gmail.com>
2023-09-13 09:47:45 -0700

    On branch master 	modified:   104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -25,7 +25,7 @@ int main(void)
 	bef1 = (bef / l);
 	bef2 = (bef % l);
 	aft1 = (aft / l);
-	afte = (aft % l);
+	aft2 = (aft % l);
 	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", aft1 + (aft2 /l));
