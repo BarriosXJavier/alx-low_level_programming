4234acbc0632339903bcd7d787f9bbded01a2db8
David Njoroge <muriithid05@gmail.com>
2023-09-13 05:53:17 -0700

    On branch master 	modified:   1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Entry point
- * print_alphabet = all alphabet in lowercase
+ * function print_alphabet = all alphabet in lowercase
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
