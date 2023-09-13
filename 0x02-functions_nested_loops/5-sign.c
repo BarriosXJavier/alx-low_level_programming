898f48930fe856133f0ce9a995a33603a163dc91
David Njoroge <muriithid05@gmail.com>
2023-09-13 07:02:28 -0700

    On branch master 	modified:   5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,13 +2,13 @@
 
 /**
  * 5-print_sign - prints a number's sign
- * Return: 1 , prints + if r is greater than 0, - if r < 0
+ * Return: 1 , prints + if n is greater than 0, - if n < 0
  */
-int print sign(int r)
+int print_sign(int n)
 {
-	if (r > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	}else if (r==0)
+	}else if (n==0)
 }
