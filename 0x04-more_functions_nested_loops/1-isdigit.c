5e2cd9a335c39e95ba0ccc4c46d393586c2a3ed7
David Njoroge <muriithid05@gmail.com>
2023-09-14 08:14:51 -0700

    On branch master 	new file:   1-isdigit.c 	modified:   main.h

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit -Check if character is a digit
+ * @x: Character to be checked
+ * Return: 1 if the character is a digit. return 0 if not
+ */
+
+int _isdigit(int x)
+
+{
+	if (x >= 48 && x <= 57)
+	{
+	return(1);
+	}
+	return(0);
+}
