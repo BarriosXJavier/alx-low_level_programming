370abd8fb496cd028062935c7b54f5817c55a8c9
David Njoroge <muriithid05@gmail.com>
2023-09-14 10:55:02 -0700

    modified:   5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@ int i, j;
 
 	for (i = 1; i <= 10; i++)
 	{
-		for(j = 0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 			_putchar(i);
-			_putchar(j %10 + '0');
+			_putchar(j % 10 + '0');
 		}
 			_putchar('\n');
 	}
