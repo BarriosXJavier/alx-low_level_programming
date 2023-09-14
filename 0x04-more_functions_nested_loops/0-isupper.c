fba0e66aa603285b9048caa34081ec15bdc8ebf6
David Njoroge <muriithid05@gmail.com>
2023-09-14 07:24:22 -0700

    On branch master 	new file:   0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Check whether a letter is upper
+ * @x: the number under check
+ * Return: 1 for upper letter or 0 if not
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
