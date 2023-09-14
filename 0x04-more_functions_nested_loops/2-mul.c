0dac7e2fa7c632b186d0a6bcc111fa0f6ad6c855
David Njoroge <muriithid05@gmail.com>
2023-09-14 09:08:25 -0700

    On branch master 	new file:   2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplying two integers
+ * @x - 1st integer
+ * @y - 2nd integer
+ * Return: The result of the two  integers
+ */
+int mul(int x, int y)
+
+{
+	return (x * y);
+}
