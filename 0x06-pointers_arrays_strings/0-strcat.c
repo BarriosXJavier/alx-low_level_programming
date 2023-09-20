19f3c4539d82cf83ee3706d16d4e82e75f4e7ace
David Njoroge <muriithid05@gmail.com>
2023-09-20 11:11:24 -0700

    new file:   0-strcat.c 	new file:   README.md 	new file:   main.h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcat - concatenating two strings
+ * @dest: input string
+ * @src: input string
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int j;
+	int k;
+
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	k = 0;
+	while (src[k] != '\0')
+	{
+		dest[j] = src[k];
+		j++;
+	}
+
+	dest[j] = '\0';
+	return (dest);
+
+}
