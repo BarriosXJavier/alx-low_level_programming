b1eda0eed920294a8b040e75cb278bb57ee9a80a
David Njoroge <muriithid05@gmail.com>
2023-09-28 13:35:10 -0700

    modified:   101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,16 +9,17 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    // If we reach the end of both strings, they are identical
+    /* If we reach the end of both strings, they are identical */
 	if (*s1 == '\0' && (*s2 == '\0' || *s2 == '*'))
 		return (1);
 
-    // If the current characters match or s2 has a wildcard
+    /* If the current characters match or s2 has a wildcard */
 	if (*s2 == '*')
+	{
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}	
+    if (*s1 == *s2)
+	return wildcmp(s1 + 1, s2 + 1);
 
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-
-		return (0);
+	return (0);
 }
